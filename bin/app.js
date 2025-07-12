3eb66a2ec7fe01e6ecf045603332c0023e2d96a5
joao-bettu <joaobettuvictor@gmail.com>
2025-07-12 13:42:21 -0300

    Ajustes no frontend

---
+++ bin/app.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Dom content loaded");
+
+    const cadastrar = document.getElementById("button-cadastrar");
+    const editar = document.getElementById("button-editar");
+    const listar = document.getElementById("button-listar");
+
+    const sections = document.getElementsByClassName("hidden");
+
+    function hideShownSections(index, sections) {
+        for (let i = 0; i < sections.length; i++){
+            if (i===index){
+                sections[i].style.display = "block";
+            } else {
+                sections[i].style.display = "none";
+            }
+        }
+    }
+
+    cadastrar.addEventListener("click", (event) => {
+        event.preventDefault();
+        hideShownSections(0, sections);
+    })
+
+    editar.addEventListener("click", (event) => {
+        event.preventDefault();
+        hideShownSections(1, sections);
+    })
+
+    listar.addEventListener("click", (event) => {
+        event.preventDefault();
+        hideShownSections(2, sections);
+    })
+});
