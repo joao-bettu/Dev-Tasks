fb9dd9489d1e4b619e858b8b77cef89805694a10
joao-bettu <joaobettuvictor@gmail.com>
2025-07-12 10:46:35 -0300

    Função listar tarefas

--- src/Tasks.php
+++ src/Tasks.php
@@ -3,6 +3,7 @@
 namespace Tasks;
 
 use PDO;
+use PDOException;
 use Tasks\DB;
 
 class Task {
@@ -22,12 +23,35 @@ class Task {
     }
 
     public function criar(array $dados){
-        $colunas = implode(', ', array_keys($dados));
-        $valores = ':' . implode(', :', array_keys($dados));
-        $sql = "INSERT INTO {$this->tabela} ($colunas) VALUES ($valores)";
-        $stmt = $this->pdo->prepare($sql);
-        return $stmt->execute($dados);
+        try {
+            $colunas = implode(', ', array_keys($dados));
+            $valores = ':' . implode(', :', array_keys($dados));
+            $sql = "INSERT INTO {$this->tabela} ($colunas) VALUES ($valores)";
+            $stmt = $this->pdo->prepare($sql);
+            return $stmt->execute($dados);
+        } catch (PDOException $e) {
+            echo "Erro criando tarefa: " . $e->getMessage() . PHP_EOL;   
+        }
     }
+
+    public function listar() {
+        try {
+            $sql = "SELECT * FROM $this->tabela";
+            $stmt = $this->pdo->query($sql);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "Erro listando as tarefas: " . $e->getMessage() . PHP_EOL;
+        }
+    }
+
+    public function editar(array $dados){
+        
+    }
+
+    public function deletar($id) {
+        
+    }
+    
 }
 
 ?>
